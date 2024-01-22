2ea1e657c75de9105393e9e8ece3f2d660cfcb3d
Alv4r0o0o0 <107275107+Alv4r0o0o0@users.noreply.github.com>
2024-01-22 20:19:05 -0300

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'proyectoverano',
+  appName: 'Phone Prime',
   webDir: 'www',
   server: {
     androidScheme: 'https'
