7838d17a0e563845cd810bfe4f52ca3217d51a8f
Nicxv <118311129+Nicxv@users.noreply.github.com>
2024-01-23 00:10:42 -0300

    cambios

--- src/app/pages/iphone/iphone.page.ts
+++ src/app/pages/iphone/iphone.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BdregistroService } from 'src/app/services/bdregistro.service';
 
 @Component({
   selector: 'app-iphone',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iphone.page.scss'],
 })
 export class IphonePage implements OnInit {
+  productos: any[] = [];
 
-  constructor() { }
+  constructor(private bd: BdregistroService) { }
 
   ngOnInit() {
+    this.bd.fetchProductos().subscribe(productos => {
+      this.productos = productos.filter(producto => producto.fk_id_categoria === 1);
+    });
   }
-
+  
 }
