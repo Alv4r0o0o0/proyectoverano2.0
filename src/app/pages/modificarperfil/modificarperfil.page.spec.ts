311662b6bf835238feec753ef0e98ca49e6e6f53
Sim0n2o03 <107275068+Sim0n2o03@users.noreply.github.com>
2024-01-23 02:17:56 -0300

    cambios

--- src/app/pages/modificarperfil/modificarperfil.page.spec.ts
+++ src/app/pages/modificarperfil/modificarperfil.page.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModificarperfilPage } from './modificarperfil.page';
-
+import { IonicModule } from '@ionic/angular';
 describe('ModificarperfilPage', () => {
   let component: ModificarperfilPage;
   let fixture: ComponentFixture<ModificarperfilPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [ModificarperfilPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
     fixture = TestBed.createComponent(ModificarperfilPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
